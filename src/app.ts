8ca1a9614d14f6a74f21edd1c5a9517288f5af5b
Rezaul Hoque <rezaul01101@gmail.com>
2024-03-03 00:21:26 +0600

    [DONE] server setup done

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}))
+
+
+//testing route
+app.get('/', (req:Request, res:Response) => {
+  res.send('Working successfully!')
+})
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+
+async function bootstrap() {
+    try{
+
+        // database connect
+        await mongoose.connect(config.database_url as string);
+        console.log('Database connected successfully!');
+
+        // server start
+        app.listen(config.port, () => {
+            console.log(`Application listening on port ${config.port}`)
+          })
+        
+    }catch(err){
+        console.log('Failed to connect');
+    }
+  
+}
+bootstrap();
