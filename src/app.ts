cd06a01769dcbaf680ed400cc8b14a02cfd8b205
Rezaul Hoque <rezaul01101@gmail.com>
2024-03-03 11:43:32 +0600

    [Bug] Prettier bug fixed

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,15 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
+app.use(cors())
 
-app.use(cors());
+app.use(express.json())
 
-app.use(express.json());
-
-app.use(express.urlencoded({extended:true}))
-
+app.use(express.urlencoded({ extended: true }))
 
 //testing route
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Working successfully!')
 })
 
-export default app;
+export default app
