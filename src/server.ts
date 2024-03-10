a42db1f60ee077e7de283c649fb23ee73d2b98d2
Rezaul Hoque <rezaul01101@gmail.com>
2024-03-10 10:51:09 +0600

    [Setup logs] Setup winston log done

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,20 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import { errorLogger, logger } from './shared/logger'
 
 async function bootstrap() {
   try {
     // database connect
     await mongoose.connect(config.database_url as string)
-    console.log('Database connected successfully!')
+    logger.info('Database connected successfully!')
 
     // server start
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`)
+      logger.info(`Application listening on port ${config.port}`)
     })
-  } catch (err) {
-    console.log('Failed to connect')
+  } catch (error) {
+    errorLogger.error('Failed to connect')
   }
 }
 bootstrap()
